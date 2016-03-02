39174210046fd18d68f87503881eb336e9b91b2e
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2016-03-02 19:26:32 +0900

    remove Promise polyfill

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 'use strict';
 
-const PinkiePromise = require('pinkie-promise');
 const writeFileAtomic = require('write-file-atomic');
 
 module.exports = function writeFileAtomically(filename, data, options) {
   if (typeof filename !== 'string') {
-    return PinkiePromise.reject(new TypeError(
+    return Promise.reject(new TypeError(
       String(filename) +
       ' is not a string. Expected a file path to write data.'
     ));
   }
 
-  return new PinkiePromise(function executor(resolve, reject) {
+  return new Promise(function executor(resolve, reject) {
     writeFileAtomic(filename, data, options, function writeFileAtomicCallback(err) {
       if (err) {
         reject(err);

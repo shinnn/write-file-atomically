2aaf3e0ebece9dbadc5b3e6d1ebb3200307411d8
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-02-16 10:40:57 +0900

    validate options object and encoding
    because the upstream write-file-atomic doesn't handle it correctly.

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
 'use strict';
 
+const inspect = require('util').inspect;
+
 const writeFileAtomic = require('write-file-atomic');
+const inspectWithKind = require('inspect-with-kind');
+const isPlainObj = require('is-plain-obj');
+
+const OPTIONS_SPEC = 'Expected the third argument to be a `write-file-atomic` option (plain <Object>) or a valid encoding (<string>)';
+const ENCODING_OPTION_SPEC = 'Expected `encoding` option to be a valid encoding (<string>)';
 
 module.exports = function writeFileAtomically(filename, data, options) {
 	if (typeof filename !== 'string') {
 		return Promise.reject(new TypeError(`${String(filename)} is not a string. Expected a file path to write data.`));
+	}
+
+	if (typeof options === 'string') {
+		if (options.length === 0) {
+			const err = new Error(`${OPTIONS_SPEC}, but got an empty string.`);
+			err.code = 'ERR_INVALID_ARG_VALUE';
+
+			return Promise.reject(err);
+		} else if (!Buffer.isEncoding(options)) {
+			const err = new Error(`${OPTIONS_SPEC}, but got an invalid encoding string ${inspect(options)}.`);
+			err.code = 'ERR_UNKNOWN_ENCODING';
+
+			return Promise.reject(err);
+		}
+
+		options = {encoding: options};
+	} else if (options) {
+		if (!isPlainObj(options)) {
+			const err = new TypeError(`${OPTIONS_SPEC}, but got ${inspectWithKind(options)}.`);
+			err.code = 'ERR_INVALID_ARG_TYPE';
+
+			return Promise.reject(err);
+		}
+
+		const encoding = options.encoding;
+
+		if (encoding === '') {
+			const err = new Error(`${ENCODING_OPTION_SPEC}, but got an empty string.`);
+			err.code = 'ERR_INVALID_OPT_VALUE_ENCODING';
+
+			return Promise.reject(err);
+		}
+
+		if (typeof encoding !== 'string') {
+			const err = new TypeError(`${ENCODING_OPTION_SPEC}, but got a non-string value ${
+				inspectWithKind(encoding)
+			}.`);
+			err.code = 'ERR_INVALID_OPT_VALUE_ENCODING';
+
+			return Promise.reject(err);
+		}
+
+		if (!Buffer.isEncoding(encoding)) {
+			const err = new Error(`Expected \`encoding\` option to be a valid encoding, but got an unknown one ${
+				inspect(encoding)
+			}.`);
+			err.code = 'ERR_INVALID_OPT_VALUE_ENCODING';
+
+			return Promise.reject(err);
+		}
 	}
 
 	return new Promise(function executor(resolve, reject) {
--- test.js
+++ test.js
@@ -10,7 +10,7 @@ const writeFileAtomically = require('.');
 const promisifiedReadFile = promisify(readFile);
 
 test('writeFileAtomically()', t => {
-	t.plan(4);
+	t.plan(11);
 	t.on('end', async () => rmfr('tmp*', {glob: true}));
 
 	(async () => {
@@ -35,7 +35,6 @@ test('writeFileAtomically()', t => {
 		);
 	})();
 
-	/*
 	(async () => {
 		const filename = 'tmp_2.txt';
 
@@ -46,13 +45,12 @@ test('writeFileAtomically()', t => {
 			'should accept a string as its third argument.'
 		);
 	})();
-	*/
 
 	const fail = t.fail.bind(t, 'Unexpectedly succceeded.');
 
 	(async () => {
 		try {
-			await writeFileAtomically(__dirname, '');
+			await writeFileAtomically(__dirname, '_');
 			fail();
 		} catch ({code}) {
 			t.equal(code, 'EISDIR', 'should fail when it cannot write a file.');
@@ -61,7 +59,7 @@ test('writeFileAtomically()', t => {
 
 	(async () => {
 		try {
-			await writeFileAtomically(1, '');
+			await writeFileAtomically(1, '_');
 			fail();
 		} catch (err) {
 			t.equal(
@@ -72,18 +70,81 @@ test('writeFileAtomically()', t => {
 		}
 	})();
 
-	/*
 	(async () => {
 		try {
-			await writeFileAtomically('tmp_3.txt', '', '123');
+			await writeFileAtomically('_', '_', new WeakMap());
+			fail();
+		} catch (err) {
+			t.equal(
+				err.toString(),
+				'TypeError: Expected the third argument to be a `write-file-atomic` option (plain <Object>) or a valid encoding (<string>), but got WeakMap {}.',
+				'should fail when it takes a non-plain options object.'
+			);
+		}
+	})();
+
+	(async () => {
+		try {
+			await writeFileAtomically('_', '_', '123');
 			fail();
 		} catch ({message}) {
 			t.equal(
 				message,
-				'Unknown encoding: 123',
-				'should fail when it takes an invalid encoding.'
+				'Expected the third argument to be a `write-file-atomic` option (plain <Object>) or a valid encoding (<string>), but got an invalid encoding string \'123\'.',
+				'should fail when it takes an invalid encoding string.'
 			);
 		}
 	})();
-	*/
+
+	(async () => {
+		try {
+			await writeFileAtomically('_', '_', '');
+			fail();
+		} catch (err) {
+			t.equal(
+				err.toString(),
+				'Error: Expected the third argument to be a `write-file-atomic` option (plain <Object>) or a valid encoding (<string>), but got an empty string.',
+				'should fail when it takes an empty encoding string.'
+			);
+		}
+	})();
+
+	(async () => {
+		try {
+			await writeFileAtomically('_', '_', {encoding: new Uint8Array()});
+			fail();
+		} catch (err) {
+			t.equal(
+				err.toString(),
+				'TypeError: Expected `encoding` option to be a valid encoding (<string>), but got a non-string value Uint8Array [  ].',
+				'should fail when it takes a non-string encoding option.'
+			);
+		}
+	})();
+
+	(async () => {
+		try {
+			await writeFileAtomically('_', '_', {encoding: ''});
+			fail();
+		} catch (err) {
+			t.equal(
+				err.toString(),
+				'Error: Expected `encoding` option to be a valid encoding (<string>), but got an empty string.',
+				'should fail when it takes an empty encoding option.'
+			);
+		}
+	})();
+
+	(async () => {
+		try {
+			await writeFileAtomically('_', '_', {encoding: 'utf7'});
+			fail();
+		} catch (err) {
+			t.equal(
+				err.toString(),
+				'Error: Expected `encoding` option to be a valid encoding, but got an unknown one \'utf7\'.',
+				'should fail when it takes an unknown encoding option.'
+			);
+		}
+	})();
 });
